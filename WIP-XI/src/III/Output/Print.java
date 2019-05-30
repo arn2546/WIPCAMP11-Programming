880e90a5fee762b351bbf8301e7056790c3a0c7d
arn2546 <arn.arn3@hotmail.com>
2019-05-30 11:37:47 +0700

    [ADD] New Project
    WIP camp วันที่ 2 สอนภาษา java

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("Arn \n");
+        
+        System.out.println("มัทฉะ");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("Title \n");
+        
+        System.out.print("Book \n");
+        
+    }
+}
